8e792cda43ac473706bd09e2b45260ab0dcec963
antoninogambino <112243266+antoninogambino@users.noreply.github.com>
2023-05-24 18:40:11 +0100

    starting generatemarkdown

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,38 +1,3 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
-
-## Description
-${data.description}
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-${data.installation}
-
-## Usage
-${data.usage}
-
-## License
-${data.license}
-
-## Contributing
-${data.contributing}
-
-## Tests
-${data.tests}
-
-## Questions
-If you have any questions, you can reach me through the following channels:
-- GitHub: [${data.github}](https://github.com/${data.github})
-- Email: ${data.email}
-`;
-}
-
-module.exports = generateMarkdown;
