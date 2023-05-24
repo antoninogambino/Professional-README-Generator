04264b751bda977a31f25f1664fedc4340d9a925
antoninogambino <112243266+antoninogambino@users.noreply.github.com>
2023-05-24 18:41:12 +0100

    adding table of contents

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -4,3 +4,26 @@ function generateMarkdown(data) {
 
 ## Description
 ${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
